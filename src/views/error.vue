024633a54a513189c54d43bdcbfe73481ed95495
hmintoh <hmintoh@gmail.com>
2021-10-28 17:02:10 +0800

    feat(views/error): create page

--- src/views/error.vue
+++ src/views/error.vue
@@ -1,11 +1,36 @@
 <template>
-  <h1>Error page pending implementation</h1>
+  <PageLayout withNavbar>
+    <h1>Oops!</h1>
+    <p class="subtitle">
+      Looks like the page you are looking for is not available. Please return to
+      the
+      <router-link title="Link to Home page" to="/" class="lambda-link">
+        Home</router-link
+      >
+      page.
+    </p>
+  </PageLayout>
 </template>
 
 <script>
+import logo from '../assets/lambda_logo.svg';
+import PageLayout from '../components/PageLayout.vue';
+
 export default {
   name: 'ViewError',
+  components: {
+    PageLayout,
+  },
+  data() {
+    return {
+      logo: logo,
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+a {
+  font-size: inherit;
+}
+</style>
