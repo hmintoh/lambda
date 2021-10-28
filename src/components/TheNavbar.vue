67a8469db563a5cee4316c51d47e65ef42749772
hmintoh <hmintoh@gmail.com>
2021-10-28 12:04:47 +0800

    feat(components/TheNavbar): create component

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="navbar">
+    <router-link title="Lambda Acoustics" to="/">
+      <img
+        :src="logo"
+        alt="lambda-logo"
+        title="Click to go back to Home page"
+        class="navbar--logo"
+      />
+    </router-link>
+
+    <nav>
+      <ul class="navbar--nav">
+        <li v-for="(route, index) in routes" :key="index">
+          <router-link
+            :title="route.label"
+            :to="route.url"
+            :class="['lambda-link', { active: activeRoute === route.url }]"
+          >
+            {{ route.label }}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+import logo from '../assets/lambda_logo.svg';
+
+export default {
+  name: 'TheNavbar',
+  data() {
+    return {
+      activeRoute: window.location.pathname,
+      logo: logo,
+      routes: [
+        { url: '/services', label: 'Services' },
+        { url: '/portfolio', label: 'Portfolio' },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+}
+
+.navbar--logo {
+  width: 60px;
+}
+
+.navbar--nav {
+  list-style: none;
+  display: flex;
+}
+
+.navbar--nav > li {
+  margin-left: var(--sp-16);
+}
+
+.active {
+  font-weight: var(--font-weight-semi);
+}
+</style>
